bb95940956962a505c5eb6fa1820c454b9c51c5a
Kevin.Z <zhaoyang@192.168.16.104>
2013-07-28 11:56:54 +0800

    Initial Commit

---
+++ Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Kevin.Z on 7/28/13.
+//  Copyright (c) 2013 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Kevin.Z on 7/28/13.
+//  Copyright (c) 2013 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QuizAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QuizAppDelegate class]));
+    }
+}
